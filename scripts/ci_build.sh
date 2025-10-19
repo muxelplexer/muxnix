ba04bd4df02ee8b057a0142deb0e020dc9480cdb
Sebastian Muxel <sebastian@muxel.dev>
2025-10-19 18:37:58 +0200

    ci: update build
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
-echo "Starting build in $(PWD)..."
+function build_image()
+{
+    echo -n "=== Building Yocto Image"
+    export MACHINE=luckfox-omni3576
+    export BITBAKEDIR=$(realpath distro/bitbake)
+    export OEROOT="$(realpath distro/layers/openembedded-core)"
+    export LAYER_ROOT="$(realpath distro/layers)"
+    export BB_ENV_PASSTHROUGH="$BB_ENV_PASSTHROUGH PATH MACHINE BITBAKEDIR OEROOT LAYER_ROOT"
+    source ./distro/layers/openembedded-core/oe-init-build-env
+    bitbake core-image-minimal
+}
 
-export MACHINE=luckfox-omni3576
-export BITBAKEDIR=$(realpath distro/bitbake)
-export OEROOT="$(realpath distro/layers/openembedded-core)"
-export LAYER_ROOT="$(realpath distro/layers)"
-export BB_ENV_PASSTHROUGH="$BB_ENV_PASSTHROUGH PATH MACHINE BITBAKEDIR OEROOT LAYER_ROOT"
-source ./distro/layers/openembedded-core/oe-init-build-env
-bitbake core-image-minimal
+build_image
