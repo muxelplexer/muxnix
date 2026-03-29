ccc115a001fc29b601c4d88811b6333c9ace37a5
Sebastian Muxel <sebastian@muxel.dev>
2026-03-29 22:29:30 +0200

    ci: pass ci dirs
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -7,7 +7,9 @@ function build_image()
     export BITBAKEDIR=$(realpath distro/bitbake)
     export OEROOT="$(realpath distro/layers/openembedded-core)"
     export LAYER_ROOT="$(realpath distro/layers)"
-    export BB_ENV_PASSTHROUGH="$BB_ENV_PASSTHROUGH PATH MACHINE BITBAKEDIR OEROOT LAYER_ROOT"
+    export BB_ENV_PASSTHROUGH="$BB_ENV_PASSTHROUGH PATH MACHINE BITBAKEDIR OEROOT LAYER_ROOT DL_DIR SSTATE_DIR"
+    export DL_DIR="/workdir/muxnix/build/downloads"
+    export SSTATE_DIR="/workdir/muxnix/build/sstate-cache"
     source ./distro/layers/openembedded-core/oe-init-build-env
     bitbake core-image-minimal
 }
