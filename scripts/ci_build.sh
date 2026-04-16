09dce9f5a4840252123a748978781f91f6961f5c
Sebastian Muxel <sebastian@muxel.dev>
2026-04-16 19:15:58 +0200

    feat(build|ci): add support for hashserver
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -12,8 +12,8 @@ function build_image()
     export SSTATE_DIR="/workdir/muxnix/build/sstate-cache"
     source ./distro/layers/openembedded-core/oe-init-build-env
     cat >> conf/local.conf << 'EOF'
-BB_HASHSERVE = ""
-BB_SIGNATURE_HANDLER = "OEBasicHash"
+BB_HASHSERVE = "ws://10.90.0.1:8687"
+BB_SIGNATURE_HANDLER = "OEEquivHash"
 SSTATE_MIRRORS += "file://.* https://sstate.muxel.dev/PATH;downloadfilename=PATH"
 EOF
     bitbake core-image-minimal
